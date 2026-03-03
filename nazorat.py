c4b9df80ea9d52b09b4e83c42a383da9b68dd034
Rahimov Yusufboy <yusufboyr1@gmail.com>
2026-03-03 14:10:45 +0500

    kompyuterdan fayl yuklandi

---
+++ nazorat.py
@@ -0,0 +1,58 @@
+# 21
+# a = int(input("sonni kiritimg: "))
+
+# if a>0:
+#     print("musbat")
+# elif a<0:
+#     print("manfiy")
+# else:
+#     print("nol")        
+
+#22
+# count = 0
+# a = 6
+# for i in range(1,a+1):
+#     if i%2==0:
+#         count=count+i
+# print(count)
+
+# # 23
+# n = int(input("son: "))
+
+# if n <=1:
+#     print("tub emas")
+# else:
+#     for i in range(2,n):
+#         if n%i==0:
+#             print("tub emas")
+#     else:
+#         print("tub son")
+
+
+# 24
+# a=10
+# for i in range(1,a+1):
+#     print(i*i)
+
+# 25
+
+# a = int(input("a= "))
+# b = int(input("b= "))
+# c = int(input("c= "))
+
+
+# if a>b and a>c:
+#     print("eng kattasi:",a)
+# elif b>a and b>c:
+#     print("eng kattasi:",b)
+# else:
+#     print("eng kattasi:",c)        
+
+
+
+
+
+
+
+
+
